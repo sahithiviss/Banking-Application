9d552aef63a2d17239620a97a2bd4ed4710a2152
Sush97 <susmithasanikommu@gmail.com>
2019-08-21 16:49:28 +0530

    modified customer rest api

--- src/main/java/com/dbs/bank/controller/CustomerController.java
+++ src/main/java/com/dbs/bank/controller/CustomerController.java
@@ -12,47 +12,42 @@ import org.springframework.web.bind.annotation.*;
 import com.dbs.bank.model.Customer;
 import com.dbs.bank.service.CustomerService;
 
-
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class CustomerController {
-	
-	@Autowired
-	CustomerService customerService;
-	
-	
-	@GetMapping("/customers")
-	public List<Customer> getAllCustomers(){
-		return customerService.listAll();
-	}
-	
-	@PostMapping("/customers")
-	public Customer createCustomer(@Valid @RequestBody Customer customer) {
-		return customerService.saveCustomer(customer);
-		
-	}
-	
-	@GetMapping("/customer/{id}")
-	public Customer getCustomerById(@PathVariable("id") Long id) {
-		return customerService.findById(id);
-	}
-	
-	@PostMapping("/customer")
-	public Optional<Customer> getCustomerByEmailAndPassword(@RequestBody Customer customer) {
-		return customerService.findByEmailAndPassword(customer.getEmail(), customer.getPassword());
-		
-	}
-	
-	@PutMapping("/customer/{id}")
-	public ResponseEntity<Customer> updateCustomer(@PathVariable("id") Long id,
-			@Valid @RequestBody Customer customerDetails) {
-		return customerService.updateCustomer(id, customerDetails);
-	}
-	
-	@DeleteMapping("/customer/{id}")
-	public ResponseEntity<?> deleteCustomer(@PathVariable("id") Long id){
-		return customerService.deleteCustomer(id);
-	}
-
+    
+    @Autowired
+    CustomerService customerService;
+    
+    @GetMapping("/customer")
+    public List<Customer> getAllCustomers(){
+        return customerService.listAll();
+    }
+    
+    @PostMapping("/customer")
+    public Customer createCustomer(@Valid @RequestBody Customer customer) {
+        return customerService.saveCustomer(customer);
+        
+    }
+    
+    @GetMapping("/customer/{id}")
+    public Customer getCustomerById(@PathVariable("id") Long id) {
+        return customerService.findById(id);
+    }
+    
+    @GetMapping("/customer/{email}/{password}")
+    public Optional<Customer> getCustomerByEmailAndPassword(@PathVariable("email") String email,@PathVariable("password") String password) {
+        return customerService.findByEmailAndPassword(email, password);
+    }
+    
+    @PutMapping("/customer")
+    public Customer updateCustomer(@RequestBody Customer customerDetails) {
+        return customerService.updateCustomer(customerDetails);
+    }
+    
+    @DeleteMapping("/customer/{id}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable("id") Long id){
+        return customerService.deleteCustomer(id);
+    }
 }
