7079f789f752e4301294f6e6a0a4350552b22bf5
kalyan <kalyankrishna01@gmail.com>
2019-08-29 14:09:34 +0530

    transaction order

--- src/main/java/com/dbs/bank/repository/TransactionRepository.java
+++ src/main/java/com/dbs/bank/repository/TransactionRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dbs.bank.model.Account;
@@ -13,6 +14,9 @@ import com.dbs.bank.model.Transaction;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
 
+	@Query(value="select * from transaction order by tid desc",nativeQuery=true)
+	List<Transaction> findAll();
+	
 	Optional<List<Transaction>> findByFromAccount(Account id);
 	
 	Optional<List<Transaction>> findByToAccount(Account id);
